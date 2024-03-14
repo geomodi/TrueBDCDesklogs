d52d483e88a2fe3142c03a32b005d39e05d67963
Alex Catro <123579701+geomodi@users.noreply.github.com>
2024-03-14 18:29:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -37,7 +37,7 @@ const dealerships = [
   { name: 'Subaru of North Miami', recordId: 'recd8VoJVWPXP0z54' }
 ];
 
-fetch('https://gist.githubusercontent.com/6060b8f6170ce8872f6364648d0f72e7/raw/dealerships.json')
+fetch('https://gist.githubusercontent.com/bf5b664819577d372545820e6dac8ee82a8d06fb/raw/dealerships.json')
   .then(response => response.json())
   .then(data => {
     const dealershipContainer = document.getElementById('dealership-container');
